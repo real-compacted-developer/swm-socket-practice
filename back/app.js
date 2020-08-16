862408ce3f09022a97a4bd0aa7adc04572c98889
Nam Koong Kwon <rndrnjs2003@gmail.com>
2020-08-16 22:29:51 +0900

    no message

--- back/app.js
+++ back/app.js
@@ -3,14 +3,12 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 
 app.use(function (req, res, next) {
   next(createError(404));
--- back/sockets/image.js
+++ back/sockets/image.js
@@ -1,10 +1,24 @@
 const SOCKET_TYPE = require("../constants/socket-type");
 // const { Slide, StudyData } = require("../models/index");
 const imagesPath = [
-  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat1.jpg",
-  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat2.jpg",
-  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/cat3.jpg",
-  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week1/puppy1.jpg"
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-01.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-02.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-03.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-04.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-05.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-06.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-07.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-08.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-09.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-10.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-11.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-12.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-13.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-14.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-15.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-16.png",
+  "https://connect-class-test.s3.ap-northeast-2.amazonaws.com/StudyGroup1/week2/test.pdf-17.png",
+
 ];
 const io = require("../bin/www").io;
 const size = imagesPath.length;
@@ -12,7 +26,6 @@ let idx = 0;
 let url = imagesPath[0];
 
 module.exports = function (socket) {
-
   socket.emit("initialize", ({ idx, url }));
   socket.on(SOCKET_TYPE.IMAGE_PREV, (data) => {
     // TODO: 자료 이전으로 넘기기
